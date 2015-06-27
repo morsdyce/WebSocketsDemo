65946aca0c36dc284733cc5ea30b8f33509b7c7d
Maayan Glikser <maayan@glikm.com>
2015-06-28 00:01:10 +0300

    Step 1

---
+++ WebSockets.Api/Controllers/TodosController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebSockets.Api.Controllers
+{
+
+    public static class TodoCollection
+    {
+        static IList<Todo> todos = new List<Todo>
+        {
+            new Todo { title = "Arrive to meetup", completed = true }
+        };
+
+        public static IList<Todo> Get()
+        {
+            return todos;
+        }
+    }
+
+    public class Todo
+    {
+        public Todo()
+        {
+            this.id = Guid.NewGuid();
+        }
+
+        public Guid id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    public class TodosController : Controller
+    {
+
+        static IList<Todo> todos;
+        public TodosController()
+        {
+            todos = TodoCollection.Get();
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Todo> Get()
+        {
+            return todos;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public Todo Post([FromBody] Todo todo)
+        {
+            if (todo != null)
+            {
+                todos.Add(todo);
+            }
+            
+            return todo;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(Guid id, [FromBody]Todo newTodo)
+        {
+            var todo = todos.Where(x => Guid.Equals(x.id, id)).FirstOrDefault();
+
+            if (todo != null && newTodo != null) {
+                todo.title = newTodo.title;
+                todo.completed = newTodo.completed;
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            var todo = todos.Where(x => x.id == id).FirstOrDefault();
+
+            if (todo != null)
+            {
+                todos.Remove(todo);
+            }
+        }
+
+        [HttpDelete]
+        public void ClearCompleted()
+        {
+            var todosToRemove = todos.Where(x => x.completed).ToList();
+
+            foreach (var todo in todosToRemove)
+            {
+                todos.Remove(todo);
+            }
+        }
+    }
+}
